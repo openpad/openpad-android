3eb9bcbcd9f888f552f6c8dadf755e9082498ea1
Ethan Madison <ethan@ethanmad.com>
2014-11-11 10:07:51 -0800

    GameAdapter.java

--- app/src/main/java/com/hw/openpad/android/model/GameAdapter.java
+++ app/src/main/java/com/hw/openpad/android/model/GameAdapter.java
@@ -1,10 +1,15 @@
 package com.hw.openpad.android.model;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.hw.openpad.android.GameConnection;
@@ -32,15 +37,36 @@ public class GameAdapter extends ArrayAdapter<GameConnection> {
         return games.size();
     }
 
+    public void setList(ArrayList<GameConnection> list){
+        games = list;
+    }
+
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater = (LayoutInflater) context
             .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View rowView = inflater.inflate(R.layout.game_cell, parent, false);
-        GameConnection gameConnection = games.get(position);
+        final GameConnection gameConnection = games.get(position);
+
         TextView gameName = (TextView) rowView.findViewById(R.id.game_name);
-        gameName.setText(gameConnection.name);
         TextView gameDesc = (TextView) rowView.findViewById(R.id.game_desc);
-        return convertView;
+        ImageView gameImage = (ImageView) rowView.findViewById((R.id.game_image));
+
+        gameName.setText(gameConnection.mName);
+        gameDesc.setText(gameConnection.mDescription);
+
+        byte[] decodedString = Base64.decode(gameConnection.mIcon.getBytes(), Base64.DEFAULT);
+        Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+        gameImage.setImageBitmap(decodedByte); // TODO make images work
+
+        rowView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                gameConnection.joinGame();
+                return true;
+            }
+        });
+
+        return rowView;
     }
 }
