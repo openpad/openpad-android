81a184fd79a77d216c49bd58ae309271bc95382f
Ethan Madison <ethan@ethanmad.com>
2014-12-03 13:08:34 -0800

    Remember user data between sessions (using SharedPreferences)

--- app/src/main/java/com/hw/openpad/android/InfoActivity.java
+++ app/src/main/java/com/hw/openpad/android/InfoActivity.java
@@ -1,7 +1,9 @@
 package com.hw.openpad.android;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -12,7 +14,8 @@ import com.hw.openpad.android.model.UserData;
 import java.util.UUID;
 
 public class InfoActivity extends Activity {
-
+    SharedPreferences sharedPref;
+    SharedPreferences.Editor editor;
     private String mFirstName, mLastName, mUsername, mUuid;
     private EditText mFirstNameText, mLastNameText, mUsernameText;
 
@@ -20,11 +23,21 @@ public class InfoActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
-        mFirstNameText = (EditText) findViewById(R.id.firstname);
-        mUsernameText = (EditText) findViewById(R.id.username);
-        mLastNameText = (EditText) findViewById(R.id.lastname);
+        sharedPref = getPreferences(Context.MODE_PRIVATE);
+        editor = sharedPref.edit();
 
-        UserData.load();
+        mFirstNameText = (EditText) findViewById(R.id.firstname);
+        mLastNameText = (EditText) findViewById(R.id.lastname);
+        mUsernameText = (EditText) findViewById(R.id.username);
+
+        UserData.load(this);
+        mFirstName = UserData.getValue("firstname");
+        mLastName = UserData.getValue("lastname");
+        mUsername = UserData.getValue("username");
+
+        mFirstNameText.setText(mFirstName);
+        mLastNameText.setText(mLastName);
+        mUsernameText.setText(mUsername);
     }
 
     public void next(View v) {
@@ -32,12 +45,14 @@ public class InfoActivity extends Activity {
         mLastName = mLastNameText.getText().toString();
         mUsername = mUsernameText.getText().toString();
         mUuid = UUID.randomUUID().toString();
+
         Log.d("PlayPhone", "generated UUID: "+ mUuid);
         UserData.put("firstname", mFirstName);
         UserData.put("lastname", mLastName);
         UserData.put("username", mUsername);
         UserData.put("phoneID", mUuid);
         UserData.put("fbuid", "");
+        UserData.save();
 
         Intent i = new Intent(this, GameListActivity.class);
         startActivity(i);
--- app/src/main/java/com/hw/openpad/android/model/UserData.java
+++ app/src/main/java/com/hw/openpad/android/model/UserData.java
@@ -1,5 +1,9 @@
 package com.hw.openpad.android.model;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -8,14 +12,25 @@ import org.json.JSONObject;
  */
 public class UserData {
     private static JSONObject data;
+    static SharedPreferences sharedPref;
+    static SharedPreferences.Editor editor;
 
-    public static void load(){
-        if(data == null){
+    public static void load(Activity activity) {
+        if (data == null) {
+            sharedPref = activity.getPreferences(Context.MODE_PRIVATE);
+
             data = new JSONObject();
+            String s = sharedPref.getString("userdata", "");
+            System.out.println("s: " + s);
+            try {
+                data = new JSONObject(s);
+            } catch (JSONException e) {
+                data = new JSONObject();
+            }
         }
     }
 
-    public static String getValue(String key){
+    public static String getValue(String key) {
         try {
             return data.getString(key);
         } catch (JSONException e) {
@@ -24,11 +39,20 @@ public class UserData {
         }
     }
 
-    public static void put(String key, String val){
+    public static void put(String key, String val) {
         try {
             data.put(key, val);
         } catch (JSONException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void save() {
+        String json = data.toString();
+        System.out.println("json: " + json);
+        editor = sharedPref.edit();
+        editor.putString("userdata", json);
+        editor.commit();
+
     }
 }
